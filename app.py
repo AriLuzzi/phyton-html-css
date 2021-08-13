4003a9839497eda8ce4121d60b1958296846430d
AriLuzzi <arienny@hotmail.com>
2021-08-13 02:19:22 +0000

    primeiro commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# imprime coisas no terminal
+print('Hello')
+
+# python é mto parecido com JS
+a = 1
+b = 2
+print(a+b)
+
+nome = input('Seu nome é? ')
+idade = input('Idade? ')
+
+
+def greeting(nome, idade)
+    print(f'Meu nome é {nome} e tenho {idade}')
+    if idade > 60:
+        print('Ta veinha ja')
+    else:
+
+
+greeting(nome, idade)
+greeting("Bruno", 33)
